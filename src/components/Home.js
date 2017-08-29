e0d05afc08d93d2c82836511493209ccae6bd53d
Jase Cutler <jasecutler@gmail.com>
2017-08-29 15:03:56 -0700

    Project complete

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return (
+            <div><h2>GO TO YOUR HOME GOLF BALL</h2>
+            <button>Push me</button>
+            </div>
+        )
+    }
+}
+
+
+export default Home
